b4366143efe6ca89c09138140aa21b1675149939
Marc Emmers <m.emmers@alfen.com>
2023-08-22 12:29:34 +0200

    Swap bind argument order and add overloads that can search the parameter in the statement
    Signed-off-by: Marc Emmers <m.emmers@alfen.com>

--- include/ocpp/common/sqlite_statement.hpp
+++ include/ocpp/common/sqlite_statement.hpp
@@ -15,55 +15,82 @@ namespace ocpp {
 class SQLiteStatement {
 private:
     sqlite3_stmt* stmt;
+    sqlite3* db;
 
 public:
-    SQLiteStatement(sqlite3* db, const std::string& query) : stmt(nullptr) {
-        if (sqlite3_prepare_v2(db, query.c_str(), query.size(), &stmt, nullptr) != SQLITE_OK) {
+    SQLiteStatement(sqlite3* db, const std::string& query) : db(db), stmt(nullptr) {
+        if (sqlite3_prepare_v2(db, query.c_str(), query.size(), &this->stmt, nullptr) != SQLITE_OK) {
             EVLOG_error << sqlite3_errmsg(db);
             EVLOG_AND_THROW(std::runtime_error("Could not prepare statement for database."));
         }
     }
 
     ~SQLiteStatement() {
-        if (stmt != nullptr) {
-            sqlite3_finalize(stmt);
+        if (this->stmt != nullptr) {
+            if (sqlite3_finalize(this->stmt) != SQLITE_OK) {
+                EVLOG_error << "Error finalizing statement: " << sqlite3_errmsg(this->db);
+            }
         }
     }
 
     sqlite3_stmt* get() const {
-        return stmt;
+        return this->stmt;
     }
 
     int step() {
-        return sqlite3_step(stmt);
+        return sqlite3_step(this->stmt);
     }
 
-    int bind_text(const std::string& str, const int idx) {
-        return sqlite3_bind_text(stmt, idx, str.c_str(), str.length(), SQLITE_TRANSIENT);
+    int bind_text(const int idx, const std::string& val) {
+        return sqlite3_bind_text(this->stmt, idx, val.c_str(), val.length(), SQLITE_TRANSIENT);
     }
 
-    int bind_int(const int val, const int idx) {
-        return sqlite3_bind_int(stmt, idx, val);
+    int bind_text(const std::string& param, const std::string& val) {
+        int index = sqlite3_bind_parameter_index(this->stmt, param.c_str());
+        if (index <= 0) {
+            throw std::out_of_range("Parameter not found in SQL query");
+        }
+        return bind_text(index, val);
+    }
+
+    int bind_int(const int idx, const int val) {
+        return sqlite3_bind_int(this->stmt, idx, val);
+    }
+
+    int bind_int(const std::string& param, const int val) {
+        int index = sqlite3_bind_parameter_index(this->stmt, param.c_str());
+        if (index <= 0) {
+            throw std::out_of_range("Parameter not found in SQL query");
+        }
+        return bind_int(index, val);
     }
 
     int bind_null(const int idx) {
-        return sqlite3_bind_null(stmt, idx);
+        return sqlite3_bind_null(this->stmt, idx);
+    }
+
+    int bind_null(const std::string& param) {
+        int index = sqlite3_bind_parameter_index(this->stmt, param.c_str());
+        if (index <= 0) {
+            throw std::out_of_range("Parameter not found in SQL query");
+        }
+        return bind_null(index);
     }
 
     int column_type(const int idx) {
-        return sqlite3_column_type(stmt, idx);
+        return sqlite3_column_type(this->stmt, idx);
     }
 
     std::string column_text(const int idx) {
-        return reinterpret_cast<const char*>(sqlite3_column_text(stmt, idx));
+        return reinterpret_cast<const char*>(sqlite3_column_text(this->stmt, idx));
     }
 
     int column_int(const int idx) {
-        return sqlite3_column_int(stmt, idx);
+        return sqlite3_column_int(this->stmt, idx);
     }
 
     double column_double(const int idx) {
-        return sqlite3_column_double(stmt, idx);
+        return sqlite3_column_double(this->stmt, idx);
     }
 };
 
