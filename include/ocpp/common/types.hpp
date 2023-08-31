77f958194d53ffc2d4af3f300cfbd253ffbf6acd
Jcoovi meha <mehafelix@gmail.com>
2023-08-31 10:28:22 -0400

    introducing measurement vs powermeter.

--- include/ocpp/common/types.hpp
+++ include/ocpp/common/types.hpp
@@ -15,7 +15,6 @@
 
 #include <date/date.h>
 #include <date/tz.h>
-
 #include <ocpp/common/cistring.hpp>
 
 using json = nlohmann::json;
@@ -223,6 +222,48 @@ struct ReactivePower {
     friend std::ostream& operator<<(std::ostream& os, const ReactivePower& k);
 };
 
+struct StateOfCharge {
+    float value;                        ///< State of Charge in percent
+    std::optional<std::string> location; ///< Location of the SoC sensor
+    /// \brief Conversion from a given StateOfCharge \p k to a given json object \p j
+    friend void to_json(json& j, const StateOfCharge& k);
+
+    /// \brief Conversion from a given json object \p j to a given StateOfCharge \p k
+    friend void from_json(const json& j, StateOfCharge& k);
+
+    // \brief Writes the string representation of the given StateOfCharge \p k to the given output stream \p os
+    /// \returns an output stream with the StateOfCharge written to
+    friend std::ostream& operator<<(std::ostream& os, const StateOfCharge& k);
+};
+
+struct Temperature {
+    float value;                ///< Temperature in degree Celsius
+    std::optional<std::string> location; ///< Location of the temperature sensor
+    /// \brief Conversion from a given Temperature \p k to a given json object \p j
+    friend void to_json(json& j, const Temperature& k);
+
+    /// \brief Conversion from a given json object \p j to a given Temperature \p k
+    friend void from_json(const json& j, Temperature& k);
+
+    // \brief Writes the string representation of the given Temperature \p k to the given output stream \p os
+    /// \returns an output stream with the Temperature written to
+    friend std::ostream& operator<<(std::ostream& os, const Temperature& k);
+};
+
+struct RPM {
+    float value;                        ///< RPM
+    std::optional<std::string> location; ///< Location of the RPM sensor
+    /// \brief Conversion from a given RPM \p k to a given json object \p j
+    friend void to_json(json& j, const RPM& k);
+
+    /// \brief Conversion from a given json object \p j to a given RPM \p k
+    friend void from_json(const json& j, RPM& k);
+
+    // \brief Writes the string representation of the given RPM \p k to the given output stream \p os
+    /// \returns an output stream with the RPM written to
+    friend std::ostream& operator<<(std::ostream& os, const RPM& k);
+};
+
 struct Powermeter {
     std::string timestamp;                  ///< Timestamp of measurement
     Energy energy_Wh_import;                ///< Imported energy in Wh (from grid)
@@ -245,6 +286,24 @@ struct Powermeter {
     // \brief Writes the string representation of the given Powermeter \p k to the given output stream \p os
     /// \returns an output stream with the Powermeter written to
     friend std::ostream& operator<<(std::ostream& os, const Powermeter& k);
+};
+
+struct Measurement {
+    Powermeter power_meter;                   ///< Powermeter data
+    std::optional<StateOfCharge> soc_Percent; ///< State of Charge in percent
+    std::optional<Temperature> temperature_C; ///< Temperature in degree Celsius
+    std::optional<RPM> rpm;                   ///< RPM
+
+    /// \brief Conversion from a given Measurement \p k to a given json object \p j
+    friend void to_json(json& j, const Measurement& k);
+
+    /// \brief Conversion from a given json object \p j to a given Measurement \p k
+    friend void from_json(const json& j, Measurement& k);
+
+    // \brief Writes the string representation of the given Measurement \p k to the given output stream \p os
+    /// \returns an output stream with the Measurement written to
+    friend std::ostream& operator<<(std::ostream& os, const Measurement& k);
+
 };
 
 enum class CertificateType {
--- lib/ocpp/common/types.cpp
+++ lib/ocpp/common/types.cpp
@@ -389,6 +389,86 @@ std::ostream& operator<<(std::ostream& os, const ReactivePower& k) {
     return os;
 }
 
+void to_json(json& j, const StateOfCharge& k) {
+    // the required parts of the type
+    j = json{
+        {"value", k.value},
+    };
+    // the optional parts of the type
+    if (k.location) {
+        j["location"] = k.location.value();
+    }
+}
+
+void from_json(const json& j, StateOfCharge& k) {
+    // the required parts of the type
+    k.value = j.at("value");
+
+    // the optional parts of the type
+    if (j.contains("location")) {
+        k.location.emplace(j.at("location"));
+    }
+}
+
+std::ostream& operator<<(std::ostream& os, const StateOfCharge& k) {
+    os << json(k).dump(4);
+    return os;
+}
+
+void to_json(json& j, const Temperature& k) {
+    // the required parts of the type
+    j = json{
+        {"value", k.value},
+    };
+
+    // the optional parts of the type
+    if (k.location) {
+        j["location"] = k.location.value();
+    }
+}
+
+void from_json(const json& j, Temperature& k) {
+    // the required parts of the type
+    k.value = j.at("value");
+
+    // the optional parts of the type
+    if (j.contains("location")) {
+        k.location.emplace(j.at("location"));
+    }
+}
+
+std::ostream& operator<<(std::ostream& os, const Temperature& k) {
+    os << json(k).dump(4);
+    return os;
+}
+
+void to_json(json& j, const RPM& k) {
+    // the required parts of the type
+    j = json{
+        {"value", k.value},
+    };
+
+    // the optional parts of the type
+    if (k.location) {
+        j["location"] = k.location.value();
+    }
+}
+
+void from_json(const json& j, RPM& k) {
+    // the required parts of the type
+    k.value = j.at("value");
+
+    // the optional parts of the type
+    if (j.contains("location")) {
+        k.location.emplace(j.at("location"));
+    }
+}
+
+std::ostream& operator<<(std::ostream& os, const RPM& k) {
+    os << json(k).dump(4);
+    return os;
+}
+
 void to_json(json& j, const Powermeter& k) {
     // the required parts of the type
     j = json{
@@ -455,6 +535,37 @@ void from_json(const json& j, Powermeter& k) {
 }
 
 std::ostream& operator<<(std::ostream& os, const Powermeter& k) {
+    os << json(k).dump(4);
+    return os;
+}
+
+void to_json(json& j, const Measurement& k) {
+    to_json(j, k.power_meter);
+    if (k.rpm) {
+        j["rpm"] = k.rpm.value();
+    }
+    if (k.soc_Percent) {
+        j["soc_Percent"] = k.soc_Percent.value();
+    }
+    if (k.temperature_C) {
+        j["temperature_C"] = k.temperature_C.value();
+    }
+}
+
+void from_json(const json& j, Measurement& k) {
+    from_json(j, k.power_meter);
+    if (j.contains("rpm")) {
+        k.rpm.emplace(j.at("rpm"));
+    }
+    if (j.contains("soc_Percent")) {
+        k.soc_Percent.emplace(j.at("soc_Percent"));
+    }
+    if (j.contains("temperature_C")) {
+        k.temperature_C.emplace(j.at("temperature_C"));
+    }
+}
+
+std::ostream& operator<<(std::ostream& os, const Measurement& k) {
     os << json(k).dump(4);
     return os;
 }
