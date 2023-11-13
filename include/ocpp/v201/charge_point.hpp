8eba655528573fd9c3c9ae05490562cf8b1c22f3
Soumya Subramanya <s.subramanya@alfen.com>
2023-11-13 13:38:26 +0100

    added callback as an option to logging
    Signed-off-by: Soumya Subramanya <s.subramanya@alfen.com>

--- include/ocpp/v201/charge_point.hpp
+++ include/ocpp/v201/charge_point.hpp
@@ -319,9 +319,7 @@ private:
 
     /// @brief Configure the message logging callback with device model parameters
     /// @param message_log_path path to file logging
-    void configure_message_logging_format(
-        const std::string& message_log_path,
-        std::function<void(const std::string& message, MessageDirection direction)> message_callback);
+    void configure_message_logging_format(const std::string& message_log_path);
 
     /* OCPP message requests */
 
