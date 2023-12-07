745241c30eb1f7c590508a7efa813b24949faded
Fabian Klemm <fabian.klemm@pionix.de>
2023-12-07 17:35:31 +0100

    surpress unused warnings
    Signed-off-by: Fabian Klemm <fabian.klemm@pionix.de>

--- include/ocpp/v201/notify_report_requests_splitter.hpp
+++ include/ocpp/v201/notify_report_requests_splitter.hpp
@@ -15,11 +15,14 @@ namespace v201 {
 class NotifyReportRequestsSplitter {
 
 private:
+    // cppcheck-suppress unused
     static const std::string MESSAGE_TYPE; // NotifyReport
     const NotifyReportRequest& original_request;
+    // cppcheck-suppress unused
     size_t max_size;
     const std::function<MessageId()>& message_id_generator_callback;
-    json request_json_template;      // json that is used  as template for request json
+    json request_json_template; // json that is used  as template for request json
+    // cppcheck-suppress unused
     const size_t json_skeleton_size; // size of the json skeleton for a call json object which includes everything
                                      // except the requests' reportData and the messageId
 
