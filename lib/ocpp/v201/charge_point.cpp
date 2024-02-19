50c5d59045ab7b3d1b970939dd03db847edf7cfb
pietfried <pietgoempel@gmail.com>
2024-02-19 12:59:49 +0100

    removed wrong invert of condition
    Signed-off-by: pietfried <pietgoempel@gmail.com>

--- lib/ocpp/v201/charge_point.cpp
+++ lib/ocpp/v201/charge_point.cpp
@@ -568,9 +568,9 @@ AuthorizeResponse ChargePoint::validate_token(const IdToken id_token, const std:
                 // C14.FR.02: If found in local list we shall start charging without an AuthorizeRequest
                 EVLOG_info << "Found valid entry in local authorization list";
                 response.idTokenInfo = id_token_info.value();
-            } else if (!this->device_model
-                            ->get_optional_value<bool>(ControllerComponentVariables::DisableRemoteAuthorization)
-                            .value_or(false)) {
+            } else if (this->device_model
+                           ->get_optional_value<bool>(ControllerComponentVariables::DisableRemoteAuthorization)
+                           .value_or(false)) {
                 EVLOG_info << "Found invalid entry in local authorization list but not sending Authorize.req because "
                               "RemoteAuthorization is disabled";
                 response.idTokenInfo.status = AuthorizationStatusEnum::Unknown;
