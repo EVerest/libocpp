466990c7ec4aa9de1e380d04b746f3f1deb86e2f
Marc Emmers <m.emmers@alfen.com>
2024-04-05 13:30:08 +0200

    Fix accidentally removed !
    Signed-off-by: Marc Emmers <m.emmers@alfen.com>

--- lib/ocpp/common/database/database_connection.cpp
+++ lib/ocpp/common/database/database_connection.cpp
@@ -20,7 +20,7 @@ DatabaseConnection::~DatabaseConnection() {
 bool DatabaseConnection::open_connection() {
     // Add special exception for databases in ram; we don't need to create a path for them
     if (this->database_file_path.string().find(":memory:") == std::string::npos and
-        fs::exists(this->database_file_path.parent_path())) {
+        !fs::exists(this->database_file_path.parent_path())) {
         fs::create_directories(this->database_file_path.parent_path());
     }
 
