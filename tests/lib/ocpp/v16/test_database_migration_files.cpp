345fbd3923ea89b19725e4da74f40fe893d92cac
Marc Emmers <m.emmers@alfen.com>
2024-04-12 14:50:12 +0200

    Extend testing for v16 migration 2
    Signed-off-by: Marc Emmers <m.emmers@alfen.com>

--- tests/lib/ocpp/v16/test_database_migration_files.cpp
+++ tests/lib/ocpp/v16/test_database_migration_files.cpp
@@ -18,13 +18,50 @@ INSTANTIATE_TEST_SUITE_P(V16, DatabaseMigrationFilesTestV16,
 TEST_P(DatabaseMigrationFilesTestV16, V16_MigrationFile2) {
     DatabaseSchemaUpdater updater{this->database.get()};
 
-    EXPECT_EQ(updater.apply_migration_files(this->migration_files_path, 2), true);
-    this->ExpectUserVersion(2);
-
-    EXPECT_EQ(this->DoesColumnExist("TRANSACTIONS", "STOP_TRANSACTION_MESSAGE_ID"), true);
-
     EXPECT_EQ(updater.apply_migration_files(this->migration_files_path, 1), true);
     this->ExpectUserVersion(1);
 
+    // Transaction table should not contain these columns yet
+    EXPECT_EQ(this->DoesColumnExist("TRANSACTIONS", "START_TRANSACTION_MESSAGE_ID"), false);
     EXPECT_EQ(this->DoesColumnExist("TRANSACTIONS", "STOP_TRANSACTION_MESSAGE_ID"), false);
+
+    // We expect to be able to insert into CONNECTORS and TRANSACTIONS table.
+    EXPECT_EQ(this->database->execute_statement("INSERT INTO CONNECTORS (ID, AVAILABILITY) VALUES (1, \"\")"), true);
+
+    std::string sql =
+        "INSERT INTO TRANSACTIONS "
+        "(ID, CONNECTOR, ID_TAG_START, TIME_START, METER_START, CSMS_ACK, METER_LAST, METER_LAST_TIME, LAST_UPDATE)"
+        " VALUES "
+        "(55, 1,         \"\",         \"\",       1,           0,        0,          \"\",            \"\")";
+    EXPECT_EQ(this->database->execute_statement(sql), true);
+
+    // After applying the migration we expect to be at version 2
+    EXPECT_EQ(updater.apply_migration_files(this->migration_files_path, 2), true);
+    this->ExpectUserVersion(2);
+
+    // We expect the added columns to exists
+    EXPECT_EQ(this->DoesColumnExist("TRANSACTIONS", "START_TRANSACTION_MESSAGE_ID"), true);
+    EXPECT_EQ(this->DoesColumnExist("TRANSACTIONS", "STOP_TRANSACTION_MESSAGE_ID"), true);
+
+    // We should be able to update the transaction we inserted earlier
+    EXPECT_EQ(this->database->execute_statement("UPDATE TRANSACTIONS SET METER_LAST=2 WHERE ID=55"), true);
+    // We should be able to update the newly introduced field too
+    EXPECT_EQ(
+        this->database->execute_statement("UPDATE TRANSACTIONS SET START_TRANSACTION_MESSAGE_ID=\"test2\" WHERE ID=55"),
+        true);
+
+    // After applying the down migration we expect to be at version 1
+    EXPECT_EQ(updater.apply_migration_files(this->migration_files_path, 1), true);
+    this->ExpectUserVersion(1);
+
+    // We expect the added columns to no longer exists
+    EXPECT_EQ(this->DoesColumnExist("TRANSACTIONS", "START_TRANSACTION_MESSAGE_ID"), false);
+    EXPECT_EQ(this->DoesColumnExist("TRANSACTIONS", "STOP_TRANSACTION_MESSAGE_ID"), false);
+
+    // We should still be able to update the transaction we inserted earlier
+    EXPECT_EQ(this->database->execute_statement("UPDATE TRANSACTIONS SET METER_LAST=2 WHERE ID=55"), true);
+    // We should not be able to update the field from version 2 any longer
+    EXPECT_EQ(
+        this->database->execute_statement("UPDATE TRANSACTIONS SET START_TRANSACTION_MESSAGE_ID=\"test2\" WHERE ID=55"),
+        false);
 }
