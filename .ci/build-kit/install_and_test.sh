cba245d001d2a1c31d65f9ecb0bd541f36a479bf
Andreas Heinrich <andreas.heinrich@rwth-aachen.de>
2024-04-12 16:37:05 +0200

    fix trap usage
    Signed-off-by: Andreas Heinrich <andreas.heinrich@rwth-aachen.de>

--- .ci/build-kit/install_and_test.sh
+++ .ci/build-kit/install_and_test.sh
@@ -2,6 +2,14 @@
 
 set -e
 
+copy_ctest_report() {
+    cp build/Testing/Temporary/LastTest.log /ext/ctest-report
+}
+
+copy_lcov_coverage() {
+    cp build/lcov_coverage.info /ext/lcov_coverage.info
+}
+
 cmake \
     -B build \
     -S "$EXT_MOUNT/source" \
@@ -13,12 +21,12 @@ cmake \
 
 ninja -j$(nproc) -C build install
 
-trap "cp build/Testing/Temporary/LastTest.log /ext/ctest-report" EXIT
-
+trap copy_ctest_report EXIT
 ninja -j$(nproc) -C build test
+trap EXIT
+copy_ctest_report
 
-trap "cp build/lcov_coverage.info /ext/lcov_coverage.info" EXIT
-
+trap copy_lcov_coverage EXIT
 ninja -j$(nproc) -C build lcov_coverage
-
-# cmake  -B build -G Ninja -DBUILD_TESTING=ON -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX="./dist" -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+trap EXIT
+copy_lcov_coverage
