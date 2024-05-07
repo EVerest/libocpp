f7ddab4280af168604ed581e39e118e35d7826b4
Gianfranco Berardi <54074967+gberardi-pillar@users.noreply.github.com>
2024-05-07 15:53:31 -0500

    Ensure each function is required in validity check.
    Signed-off-by: Gianfranco Berardi <54074967+gberardi-pillar@users.noreply.github.com>

--- tests/lib/ocpp/v201/test_charge_point.cpp
+++ tests/lib/ocpp/v201/test_charge_point.cpp
@@ -4,32 +4,126 @@
 
 namespace ocpp::v201 {
 
-TEST(ChargePointFixture, CallbacksAreValidWhenAllRequiredCallbacksProvided) {
-    ocpp::v201::Callbacks callbacks;
-    EXPECT_FALSE(callbacks.all_callbacks_valid());
+class ChargePointFixture : public testing::Test {
+public:
+    ChargePointFixture() {
+    }
+    ~ChargePointFixture() {
+    }
 
-    testing::MockFunction<bool(const std::optional<const int32_t> evse_id, const ResetEnum& reset_type)> is_reset_allowed_callback_mock;
-    testing::MockFunction<void(const std::optional<const int32_t> evse_id, const ResetEnum& reset_type)> reset_callback_mock;
+    void configure_callbacks_with_mocks() {
+        callbacks.is_reset_allowed_callback = is_reset_allowed_callback_mock.AsStdFunction();
+        callbacks.reset_callback = reset_callback_mock.AsStdFunction();
+        callbacks.stop_transaction_callback = stop_transaction_callback_mock.AsStdFunction();
+        callbacks.pause_charging_callback = pause_charging_callback_mock.AsStdFunction();
+        callbacks.connector_effective_operative_status_changed_callback =
+            connector_effective_operative_status_changed_callback_mock.AsStdFunction();
+        callbacks.get_log_request_callback = get_log_request_callback_mock.AsStdFunction();
+        callbacks.unlock_connector_callback = unlock_connector_callback_mock.AsStdFunction();
+        callbacks.remote_start_transaction_callback = remote_start_transaction_callback_mock.AsStdFunction();
+        callbacks.is_reservation_for_token_callback = is_reservation_for_token_callback_mock.AsStdFunction();
+        callbacks.update_firmware_request_callback = update_firmware_request_callback_mock.AsStdFunction();
+    }
+
+    testing::MockFunction<bool(const std::optional<const int32_t> evse_id, const ResetEnum& reset_type)>
+        is_reset_allowed_callback_mock;
+    testing::MockFunction<void(const std::optional<const int32_t> evse_id, const ResetEnum& reset_type)>
+        reset_callback_mock;
     testing::MockFunction<void(const int32_t evse_id, const ReasonEnum& stop_reason)> stop_transaction_callback_mock;
     testing::MockFunction<void(const int32_t evse_id)> pause_charging_callback_mock;
-    testing::MockFunction<void(const int32_t evse_id, const int32_t connector_id, const OperationalStatusEnum new_status)> connector_effective_operative_status_changed_callback_mock;
+    testing::MockFunction<void(const int32_t evse_id, const int32_t connector_id,
+                               const OperationalStatusEnum new_status)>
+        connector_effective_operative_status_changed_callback_mock;
     testing::MockFunction<GetLogResponse(const GetLogRequest& request)> get_log_request_callback_mock;
-    testing::MockFunction<UnlockConnectorResponse(const int32_t evse_id, const int32_t connecor_id)> unlock_connector_callback_mock;
-    testing::MockFunction<void(const RequestStartTransactionRequest& request, const bool authorize_remote_start)> remote_start_transaction_callback_mock;
-    testing::MockFunction<bool(const int32_t evse_id, const CiString<36> idToken, const std::optional<CiString<36>> groupIdToken)> is_reservation_for_token_callback_mock;
-    testing::MockFunction<UpdateFirmwareResponse(const UpdateFirmwareRequest& request)> update_firmware_request_callback_mock;
+    testing::MockFunction<UnlockConnectorResponse(const int32_t evse_id, const int32_t connecor_id)>
+        unlock_connector_callback_mock;
+    testing::MockFunction<void(const RequestStartTransactionRequest& request, const bool authorize_remote_start)>
+        remote_start_transaction_callback_mock;
+    testing::MockFunction<bool(const int32_t evse_id, const CiString<36> idToken,
+                               const std::optional<CiString<36>> groupIdToken)>
+        is_reservation_for_token_callback_mock;
+    testing::MockFunction<UpdateFirmwareResponse(const UpdateFirmwareRequest& request)>
+        update_firmware_request_callback_mock;
+    ocpp::v201::Callbacks callbacks;
+};
 
-    callbacks.is_reset_allowed_callback = is_reset_allowed_callback_mock.AsStdFunction();
-    callbacks.reset_callback = reset_callback_mock.AsStdFunction();
-    callbacks.stop_transaction_callback = stop_transaction_callback_mock.AsStdFunction();
-    callbacks.pause_charging_callback = pause_charging_callback_mock.AsStdFunction();
-    callbacks.connector_effective_operative_status_changed_callback = connector_effective_operative_status_changed_callback_mock.AsStdFunction();
-    callbacks.get_log_request_callback = get_log_request_callback_mock.AsStdFunction();
-    callbacks.unlock_connector_callback = unlock_connector_callback_mock.AsStdFunction();
-    callbacks.remote_start_transaction_callback = remote_start_transaction_callback_mock.AsStdFunction();
-    callbacks.is_reservation_for_token_callback = is_reservation_for_token_callback_mock.AsStdFunction();
-    callbacks.update_firmware_request_callback = update_firmware_request_callback_mock.AsStdFunction();
+TEST_F(ChargePointFixture, CallbacksAreInvalidWhenNotProvided) {
+    EXPECT_FALSE(callbacks.all_callbacks_valid());
+}
 
+TEST_F(ChargePointFixture, CallbacksAreValidWhenAllRequiredCallbacksProvided) {
+    configure_callbacks_with_mocks();
     EXPECT_TRUE(callbacks.all_callbacks_valid());
 }
+
+TEST_F(ChargePointFixture, CallbacksValidityChecksIfResetIsAllowedCallbackExists) {
+    configure_callbacks_with_mocks();
+    callbacks.is_reset_allowed_callback = nullptr;
+
+    EXPECT_FALSE(callbacks.all_callbacks_valid());
 }
+
+TEST_F(ChargePointFixture, CallbacksValidityChecksIfResetCallbackExists) {
+    configure_callbacks_with_mocks();
+    callbacks.reset_callback = nullptr;
+
+    EXPECT_FALSE(callbacks.all_callbacks_valid());
+}
+
+TEST_F(ChargePointFixture, CallbacksValidityChecksIfStopTransactionCallbackExists) {
+    configure_callbacks_with_mocks();
+    callbacks.stop_transaction_callback = nullptr;
+
+    EXPECT_FALSE(callbacks.all_callbacks_valid());
+}
+
+TEST_F(ChargePointFixture, CallbacksValidityChecksIfPauseChargingCallbackExists) {
+    configure_callbacks_with_mocks();
+    callbacks.pause_charging_callback = nullptr;
+
+    EXPECT_FALSE(callbacks.all_callbacks_valid());
+}
+
+TEST_F(ChargePointFixture, CallbacksValidityChecksIfConnectorEffectiveOperativeStatusChangedCallbackExists) {
+    configure_callbacks_with_mocks();
+    callbacks.connector_effective_operative_status_changed_callback = nullptr;
+
+    EXPECT_FALSE(callbacks.all_callbacks_valid());
+}
+
+TEST_F(ChargePointFixture, CallbacksValidityChecksIfGetLogRequestCallbackExists) {
+    configure_callbacks_with_mocks();
+    callbacks.get_log_request_callback = nullptr;
+
+    EXPECT_FALSE(callbacks.all_callbacks_valid());
+}
+
+TEST_F(ChargePointFixture, CallbacksValidityChecksIfUnlockConnectorCallbackExists) {
+    configure_callbacks_with_mocks();
+    callbacks.unlock_connector_callback = nullptr;
+
+    EXPECT_FALSE(callbacks.all_callbacks_valid());
+}
+
+TEST_F(ChargePointFixture, CallbacksValidityChecksIfRemoteStartTransactionCallbackExists) {
+    configure_callbacks_with_mocks();
+    callbacks.remote_start_transaction_callback = nullptr;
+
+    EXPECT_FALSE(callbacks.all_callbacks_valid());
+}
+
+TEST_F(ChargePointFixture, CallbacksValidityChecksIfIsReservationForTokenCallbackExists) {
+    configure_callbacks_with_mocks();
+    callbacks.is_reservation_for_token_callback = nullptr;
+
+    EXPECT_FALSE(callbacks.all_callbacks_valid());
+}
+
+TEST_F(ChargePointFixture, CallbacksValidityChecksIfUpdateFirmwareRequestCallbackExists) {
+    configure_callbacks_with_mocks();
+    callbacks.update_firmware_request_callback = nullptr;
+
+    EXPECT_FALSE(callbacks.all_callbacks_valid());
+}
+
+} // namespace ocpp::v201
