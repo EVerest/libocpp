d44c9ae07a355a4fbaf7620a3914f5c68db17be5
AssemblyJohn <ioan.bogdann@gmail.com>
2024-07-01 13:10:14 +0300

    Allow monitors with any value if delta/periodic of non numeric type
    Signed-off-by: AssemblyJohn <ioan.bogdann@gmail.com>

--- include/ocpp/v201/device_model.hpp
+++ include/ocpp/v201/device_model.hpp
@@ -64,6 +64,19 @@ template <DataEnum T> auto to_specific_type_auto(const std::string& value) {
     }
 }
 
+template<DataEnum T> bool is_type_numeric() {
+    static_assert(T == DataEnum::string || T == DataEnum::integer || T == DataEnum::decimal ||
+                      T == DataEnum::dateTime || T == DataEnum::boolean || T == DataEnum::OptionList || 
+                      T == DataEnum::SequenceList || T == DataEnum::MemberList,
+                  "Requested unknown datatype");
+
+    if constexpr (T == DataEnum::integer || T == DataEnum::decimal) {
+        return true;
+    } else {
+        return false;
+    }    
+}
+
 typedef std::function<void(const std::unordered_map<int64_t, VariableMonitoringMeta>& monitors,
                            const Component& component, const Variable& variable,
                            const VariableCharacteristics& characteristics, const std::string& value_previous,
