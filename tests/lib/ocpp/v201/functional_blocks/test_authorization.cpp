8cdc9ddac28121b617d5e53e811bb9fb661da960
Maaike Zijderveld, iolar <git.mail@iolar.nl>
2025-01-03 19:21:21 +0100

    Make database handler mock and add tests for authorization.
    Signed-off-by: Maaike Zijderveld, iolar <git.mail@iolar.nl>

--- tests/lib/ocpp/v201/functional_blocks/test_authorization.cpp
+++ tests/lib/ocpp/v201/functional_blocks/test_authorization.cpp
@@ -11,21 +11,24 @@
 #include "connectivity_manager_mock.hpp"
 #include "device_model_test_helper.hpp"
 #include "message_dispatcher_mock.hpp"
+#include "mocks/database_handler_mock.hpp"
 
 using namespace ocpp::v201;
+using ::testing::_;
+using ::testing::Return;
 
 class AuthorizationTest : public ::testing::Test {
 public:
 protected: // Members
     DeviceModelTestHelper device_model_test_helper;
     MockMessageDispatcher mock_dispatcher;
-    std::unique_ptr<DeviceModel> device_model;
-    ConnectivityManagerMock connectivity_manager;
-    std::shared_ptr<DatabaseHandler> database_handler;
+    DeviceModel* device_model;
+    ::testing::NiceMock<ConnectivityManagerMock> connectivity_manager;
+    ::testing::NiceMock<std::shared_ptr<ocpp::v201::DatabaseHandlerMock>> database_handler_mock;
 
     // Authorization is a unique ptr because we first need to create the database handler before making authorization,
     // and the database handler is not created in the initializer list. So we have to be able to create Authorization
-    // later.
+    // later. // TODO mz this might be changed??
     std::unique_ptr<Authorization> authorization;
 
 protected: // Functions
@@ -34,19 +37,12 @@ protected: // Functions
         mock_dispatcher(),
         device_model(device_model_test_helper.get_device_model()),
         connectivity_manager(),
-        database_handler(nullptr),
-        authorization(nullptr) {
-        std::unique_ptr<ocpp::common::DatabaseConnection> database_connection =
-            std::make_unique<ocpp::common::DatabaseConnection>(DEVICE_MODEL_DB_IN_MEMORY_PATH);
-        database_connection->open_connection();
-        database_handler = std::make_shared<DatabaseHandler>(std::move(database_connection), MIGRATION_FILES_PATH);
-        authorization = std::make_unique<Authorization>(mock_dispatcher, *device_model.get(), connectivity_manager,
-                                                        database_handler);
+        database_handler_mock(std::make_shared<ocpp::v201::DatabaseHandlerMock>()),
+        authorization(std::make_unique<Authorization>(mock_dispatcher, *device_model, connectivity_manager,
+                                                      database_handler_mock)) {
     }
 
-    ~AuthorizationTest () {
-        database_handler = nullptr;
-        authorization = nullptr;
+    ~AuthorizationTest() {
     }
 
     ///
@@ -60,13 +56,101 @@ protected: // Functions
                                           AttributeEnum::Actual, enabled ? "true" : "false", "default", true),
                   SetVariableStatusEnum::Accepted);
     }
+
+    IdToken get_id_token(const std::string& token = "VALID_ID_TOKEN",
+                         const IdTokenEnum token_type = IdTokenEnum::ISO14443) {
+        IdToken id_token;
+        id_token.idToken = token;
+        id_token.type = token_type;
+        return id_token;
+    }
+
+    ocpp::EnhancedMessage<MessageType>
+    create_example_authorize_response(const std::optional<AuthorizeCertificateStatusEnum> certificate_status,
+                                      const AuthorizationStatusEnum& status) {
+        AuthorizeResponse response;
+        response.certificateStatus = certificate_status;
+        IdTokenInfo id_token_info;
+        id_token_info.status = status;
+        response.idTokenInfo = id_token_info;
+        ocpp::CallResult<AuthorizeResponse> call_result(response, "uniqueId");
+        ocpp::EnhancedMessage<MessageType> enhanced_message;
+        enhanced_message.messageType = MessageType::AuthorizeResponse;
+        enhanced_message.message = call_result;
+        return enhanced_message;
+    }
 };
 
 TEST_F(AuthorizationTest, is_auth_cache_ctrlr_enabled) {
-    EXPECT_TRUE(true);
-    // set_auth_cache_enabled(this->device_model.get(), false);
-    // EXPECT_FALSE(authorization ->is_auth_cache_ctrlr_enabled());
+    set_auth_cache_enabled(this->device_model, false);
+    EXPECT_FALSE(authorization->is_auth_cache_ctrlr_enabled());
 
-    // set_auth_cache_enabled(this->device_model.get(), true);
-    // EXPECT_TRUE(authorization ->is_auth_cache_ctrlr_enabled());
+    set_auth_cache_enabled(this->device_model, true);
+    EXPECT_TRUE(authorization->is_auth_cache_ctrlr_enabled());
+}
+
+TEST_F(AuthorizationTest, authorize_req_websocket_disconnected) {
+    ON_CALL(this->connectivity_manager, is_websocket_connected()).WillByDefault(Return(false));
+    const AuthorizeResponse response = authorization->authorize_req(get_id_token(), std::nullopt, std::nullopt);
+    EXPECT_EQ(response.idTokenInfo.status, AuthorizationStatusEnum::Unknown);
+}
+
+TEST_F(AuthorizationTest, authorize_req_wrong_future_message_type) {
+    ON_CALL(this->connectivity_manager, is_websocket_connected()).WillByDefault(Return(true));
+    ocpp::EnhancedMessage<MessageType> enhanced_message;
+    enhanced_message.messageType = MessageType::GetDisplayMessages;
+    EXPECT_CALL(mock_dispatcher, dispatch_call_async(_, _))
+        .WillOnce(Return(std::async(std::launch::deferred, [enhanced_message]() { return enhanced_message; })));
+
+    const AuthorizeResponse response = authorization->authorize_req(get_id_token(), std::nullopt, std::nullopt);
+    EXPECT_EQ(response.idTokenInfo.status, AuthorizationStatusEnum::Unknown);
+}
+
+TEST_F(AuthorizationTest, authorize_req_accepted) {
+    ON_CALL(this->connectivity_manager, is_websocket_connected()).WillByDefault(Return(true));
+    EXPECT_CALL(mock_dispatcher, dispatch_call_async(_, _)).WillOnce(Return(std::async(std::launch::deferred, [this]() {
+        return create_example_authorize_response(AuthorizeCertificateStatusEnum::Accepted,
+                                                 AuthorizationStatusEnum::Accepted);
+    })));
+
+    const AuthorizeResponse response = authorization->authorize_req(get_id_token(), std::nullopt, std::nullopt);
+    EXPECT_EQ(response.idTokenInfo.status, AuthorizationStatusEnum::Accepted);
+}
+
+TEST_F(AuthorizationTest, authorize_req_exception) {
+    ON_CALL(this->connectivity_manager, is_websocket_connected()).WillByDefault(Return(true));
+    EXPECT_CALL(mock_dispatcher, dispatch_call_async(_, _)).WillOnce(Return(std::async(std::launch::deferred, [this]() {
+        return create_example_authorize_response(AuthorizeCertificateStatusEnum::Accepted,
+                                                 static_cast<AuthorizationStatusEnum>(INT32_MAX));
+    })));
+
+    const AuthorizeResponse response = authorization->authorize_req(get_id_token(), std::nullopt, std::nullopt);
+    EXPECT_EQ(response.idTokenInfo.status, AuthorizationStatusEnum::Unknown);
+}
+
+TEST_F(AuthorizationTest, authorize_req_exception2) {
+    ON_CALL(this->connectivity_manager, is_websocket_connected()).WillByDefault(Return(true));
+    EXPECT_CALL(mock_dispatcher, dispatch_call_async(_, _)).WillOnce(Return(std::async(std::launch::deferred, [this]() {
+        return create_example_authorize_response(static_cast<AuthorizeCertificateStatusEnum>(INT32_MAX),
+                                                 AuthorizationStatusEnum::Accepted);
+    })));
+
+    const AuthorizeResponse response = authorization->authorize_req(get_id_token(), std::nullopt, std::nullopt);
+    EXPECT_EQ(response.idTokenInfo.status, AuthorizationStatusEnum::Unknown);
+}
+
+TEST_F(AuthorizationTest, update_authorization_cache_size) {
+    auto& auth_cache_size = ControllerComponentVariables::AuthCacheStorage;
+    this->device_model->set_read_only_value(auth_cache_size.component, auth_cache_size.variable.value(),
+                                            AttributeEnum::Actual, "42", "test");
+    std::optional<int> size = device_model->get_optional_value<int>(auth_cache_size, AttributeEnum::Actual);
+    ASSERT_TRUE(size.has_value());
+    EXPECT_EQ(size.value(), 42);
+
+    EXPECT_CALL(*this->database_handler_mock, authorization_cache_get_binary_size()).WillRepeatedly(Return(35));
+    this->authorization->update_authorization_cache_size();
+
+    size = device_model->get_optional_value<int>(auth_cache_size, AttributeEnum::Actual);
+    ASSERT_TRUE(size.has_value());
+    EXPECT_EQ(size.value(), 35);
 }
