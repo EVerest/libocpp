4948afd489534ccce71ad9155d4f2a674cd8e170
Marc Emmers <m.emmers@alfen.com>
2025-01-13 09:20:07 +0100

    Create interfaces for the database and add a mock and a fake
    Signed-off-by: Marc Emmers <m.emmers@alfen.com>

---
+++ tests/lib/ocpp/v201/mocks/database_handler_fake.hpp
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2020 - 2025 Pionix GmbH and Contributors to EVerest
+
+#pragma once
+
+#include "database_handler_mock.hpp"
+#include <ocpp/v201/database_handler.hpp>
+
+namespace ocpp::v201 {
+class DatabaseHandlerFake : public DatabaseHandlerMock {
+    DatabaseHandler handler;
+
+public:
+    DatabaseHandlerFake(std::unique_ptr<common::DatabaseConnectionInterface> database,
+                        const fs::path& sql_migration_files_path) :
+        handler(std::move(database), sql_migration_files_path) {
+
+        // Add more wrappers here as needed
+        ON_CALL(*this, open_connection).WillByDefault([this]() { handler.open_connection(); });
+        ON_CALL(*this, new_statement).WillByDefault([this](const std::string& sql) { return handler.new_statement(sql); });
+    }
+};
+} // namespace ocpp::v201
