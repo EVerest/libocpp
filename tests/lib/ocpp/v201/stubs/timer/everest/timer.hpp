1720258f0e53191dea93fe7752c6e68d614107bb
Maaike Zijderveld, iolar <git.mail@iolar.nl>
2025-01-21 16:17:28 +0100

    Remove year from copyright.
    Signed-off-by: Maaike Zijderveld, iolar <git.mail@iolar.nl>

--- tests/lib/ocpp/v201/stubs/timer/everest/timer.hpp
+++ tests/lib/ocpp/v201/stubs/timer/everest/timer.hpp
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: Apache-2.0
-// Copyright 2020 - 2021 Pionix GmbH and Contributors to EVerest
+// Copyright Pionix GmbH and Contributors to EVerest
 
 ///
 /// @file timer.hpp
