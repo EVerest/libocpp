a232696cbca7b6f4be3685272a5aec055eb86e8b
Marc Emmers <35759328+marcemmers@users.noreply.github.com>
2025-01-29 15:12:44 +0100

    Fix clang-format
    Signed-off-by: Marc Emmers <35759328+marcemmers@users.noreply.github.com>

--- lib/ocpp/v201/charge_point.cpp
+++ lib/ocpp/v201/charge_point.cpp
@@ -842,7 +842,7 @@ void ChargePoint::message_callback(const std::string& message) {
             CallError(MessageId("-1"), "RpcFrameworkError", e.what(), json({})));
         const auto& security_event = ocpp::security_events::INVALIDMESSAGES;
         this->security->security_event_notification_req(CiString<50>(security_event), CiString<255>(message), true,
-                                              utils::is_critical(security_event));
+                                                        utils::is_critical(security_event));
         return;
     } catch (const EnumConversionException& e) {
         EVLOG_error << "EnumConversionException during handling of message: " << e.what();
