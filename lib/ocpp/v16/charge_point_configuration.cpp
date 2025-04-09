e38477dce550e31ef28744dd22c1b0c64cbf722e
Piet Gömpel <37657534+Pietfried@users.noreply.github.com>
2025-04-09 19:48:47 +0200

    Fixed bug to set CentralContractValidationAllowed (#1038)
    Signed-off-by: Piet Gömpel <pietgoempel@gmail.com>

--- lib/ocpp/v16/charge_point_configuration.cpp
+++ lib/ocpp/v16/charge_point_configuration.cpp
@@ -3656,7 +3656,7 @@ ConfigurationStatus ChargePointConfiguration::set(CiString<50> key, CiString<500
         if (this->getCentralContractValidationAllowed() == std::nullopt) {
             return ConfigurationStatus::NotSupported;
         } else {
-            this->setContractValidationOffline(ocpp::conversions::string_to_bool(value.get()));
+            this->setCentralContractValidationAllowed(ocpp::conversions::string_to_bool(value.get()));
         }
     }
     if (key == "CertSigningWaitMinimum") {
