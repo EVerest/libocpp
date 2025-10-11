b21fbeeaec136056e946010c4fcdf667650e728c
Kai-Uwe Hermann <kai-uwe.hermann@pionix.de>
2025-10-11 23:53:59 +0200

    Modify drop_transaction_data algorithm to fix some clang-tidy issues
    Keeping the behavior the same, but the algorithm looks suspicious
    
    Signed-off-by: Kai-Uwe Hermann <kai-uwe.hermann@pionix.de>

--- lib/ocpp/v16/utils.cpp
+++ lib/ocpp/v16/utils.cpp
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: Apache-2.0
 // Copyright Pionix GmbH and Contributors to EVerest
 
+#include <iterator>
 #include <ocpp/common/utils.hpp>
 #include <ocpp/v16/utils.hpp>
 
@@ -12,11 +13,16 @@ size_t get_message_size(const ocpp::Call<StopTransactionRequest>& call) {
     return json(call).at(CALL_PAYLOAD).dump().length();
 }
 
+// FIXME: this algorithm seems broken
 void drop_transaction_data(size_t max_message_size, ocpp::Call<StopTransactionRequest>& call) {
-    auto& transaction_data = call.msg.transactionData.value();
-    while (get_message_size(call) > max_message_size && transaction_data.size() > 2) {
-        for (size_t i = 1; i < transaction_data.size() - 1; i = i + 2) {
-            transaction_data.erase(transaction_data.begin() + i);
+    if (call.msg.transactionData.has_value()) {
+        auto& transaction_data = call.msg.transactionData.value();
+        while (get_message_size(call) > max_message_size && transaction_data.size() > 2) {
+            auto transaction_data_it = transaction_data.begin();
+            for (size_t i = 1; i < transaction_data.size() - 1; i = i + 2) {
+                std::advance(transaction_data_it, i);
+                transaction_data.erase(transaction_data_it);
+            }
         }
     }
 }
