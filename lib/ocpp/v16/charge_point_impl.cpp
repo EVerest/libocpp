EVerest/everest-core

DataTransfer.req(GetInstalledCertificates) now responds with NotFound in case no certificates were found (#1035)
Signed-off-by: Piet Gömpel <pietgoempel@gmail.com>

lib/ocpp/v16/charge_point_impl.cpp
@@ -3940,15 +3940,18 @@ void ChargePointImpl::handle_data_transfer_pnc_get_installed_certificates(Call<D
 
             const auto certificate_hash_data_chains =
                 this->evse_security->get_installed_certificates(certificate_types);
-            std::optional<std::vector<ocpp::v2::CertificateHashDataChain>> certificate_hash_data_chain_v2_opt;
-            std::vector<ocpp::v2::CertificateHashDataChain> certificate_hash_data_chain_v2;
-            for (const auto certificate_hash_data_chain_entry : certificate_hash_data_chains) {
-                certificate_hash_data_chain_v2.push_back(
-                    ocpp::evse_security_conversions::to_ocpp_v2(certificate_hash_data_chain_entry));
+
+            if (!certificate_hash_data_chains.empty()) {
+                std::optional<std::vector<ocpp::v2::CertificateHashDataChain>> certificate_hash_data_chain_v2_opt;
+                std::vector<ocpp::v2::CertificateHashDataChain> certificate_hash_data_chain_v2;
+                for (const auto certificate_hash_data_chain_entry : certificate_hash_data_chains) {
+                    certificate_hash_data_chain_v2.push_back(
+                        ocpp::evse_security_conversions::to_ocpp_v2(certificate_hash_data_chain_entry));
+                }
+                certificate_hash_data_chain_v2_opt.emplace(certificate_hash_data_chain_v2);
+                get_certificate_ids_response.certificateHashDataChain = certificate_hash_data_chain_v2_opt;
+                get_certificate_ids_response.status = ocpp::v2::GetInstalledCertificateStatusEnum::Accepted;
             }
-            certificate_hash_data_chain_v2_opt.emplace(certificate_hash_data_chain_v2);
-            get_certificate_ids_response.certificateHashDataChain = certificate_hash_data_chain_v2_opt;
-            get_certificate_ids_response.status = ocpp::v2::GetInstalledCertificateStatusEnum::Accepted;
 
             response.data.emplace(json(get_certificate_ids_response).dump());
         } else {

EVerest/libocpp

lib/ocpp/v16/charge_point_impl.cpp
@@ -4036,15 +4036,18 @@ void ChargePointImpl::handle_data_transfer_pnc_get_installed_certificates(Call<D
 
             const auto certificate_hash_data_chains =
                 this->evse_security->get_installed_certificates(certificate_types);
-            std::optional<std::vector<ocpp::v201::CertificateHashDataChain>> certificate_hash_data_chain_v201_opt;
-            std::vector<ocpp::v201::CertificateHashDataChain> certificate_hash_data_chain_v201;
-            for (const auto certificate_hash_data_chain_entry : certificate_hash_data_chains) {
-                certificate_hash_data_chain_v201.push_back(
-                    ocpp::evse_security_conversions::to_ocpp_v201(certificate_hash_data_chain_entry));
+
+            if (!certificate_hash_data_chains.empty()) {
+                std::optional<std::vector<ocpp::v201::CertificateHashDataChain>> certificate_hash_data_chain_v2_opt;
+                std::vector<ocpp::v201::CertificateHashDataChain> certificate_hash_data_chain_v2;
+                for (const auto certificate_hash_data_chain_entry : certificate_hash_data_chains) {
+                    certificate_hash_data_chain_v2.push_back(
+                        ocpp::evse_security_conversions::to_ocpp_v2(certificate_hash_data_chain_entry));
+                }
+                certificate_hash_data_chain_v2_opt.emplace(certificate_hash_data_chain_v2);
+                get_certificate_ids_response.certificateHashDataChain = certificate_hash_data_chain_v2_opt;
+                get_certificate_ids_response.status = ocpp::v201::GetInstalledCertificateStatusEnum::Accepted;
             }
-            certificate_hash_data_chain_v201_opt.emplace(certificate_hash_data_chain_v201);
-            get_certificate_ids_response.certificateHashDataChain = certificate_hash_data_chain_v201_opt;
-            get_certificate_ids_response.status = ocpp::v201::GetInstalledCertificateStatusEnum::Accepted;
 
             response.data.emplace(json(get_certificate_ids_response).dump());
         } else {